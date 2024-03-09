42e301ec117ac397c118d30c1c9cc9f5ac3d7e4f
tangx <uyinn28@gmail.com>
2024-03-09 17:28:02 +0800

    4. 使用 useRef 获取 input 框中的值， 并写入 state 中

--- src/componenets/TodoList/Input/index.tsx
+++ src/componenets/TodoList/Input/index.tsx
@@ -3,26 +3,36 @@
 import { addTodo } from "../../../store/modules/todoStore";
 import { useDispatch } from "react-redux";
 import { ToDo } from "../../types";
-
+import { useRef } from "react";
 
 export default () => {
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const dispatch = useDispatch();
 
-
   const addItem = () => {
+    // 如果值为空，不添加
+    if (!inputRef.current?.value) {
+      return
+    }
 
     const item: ToDo = {
       id: Date.now(),
-      title: "Learn Redux Toolkit",
+      title: inputRef.current?.value,
       completed: false
     }
 
+    // dispath 调用 action 添加新条目
     dispatch(addTodo(item))
+    // 添加完成后清空 input 框
+    inputRef.current!.value = ""
   }
 
   return (
     <>
-      <input type="text" placeholder="输入任务" />
+      <input ref={inputRef}
+        type="text" placeholder="输入任务" />
       <button
         onClick={() => addItem()}
       >添加</button>
